d43a13609ec206fedbd4689ed70995856397da5b
caian <caianpedroso@gmail.com>
2023-10-03 16:11:55 -0300

    Create class VeganDishBuilder

---
+++ src/creational/builder/classes/vegan-dish-builder.ts
@@ -0,0 +1,27 @@
+import { MealCompositeProtocol } from "../interfaces/meal-composite-protocol";
+import { MealBox } from "./meal-box";
+import { Rice, Beans } from "./meals";
+
+export class VeganDishBuilder implements  MealCompositeProtocol {
+    private _meal: MealBox = new MealBox();
+
+    reset(): this {
+        this._meal = new MealBox();
+        return this;
+    }
+
+    makeMeal(): this {
+        const rice = new Rice('Arroz', 5);
+        const beans = new Beans('Feijão', 10);
+        this._meal.add(rice, beans);
+        return this;
+    }
+
+    getMeal(): MealBox {
+        return this._meal;
+    }
+
+    getPrice(): number {
+        return this._meal.getPrice();
+    }
+}
