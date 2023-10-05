2bbf903583d672ca70095d6507ab8b4df9c77a20
caian <caianpedroso@gmail.com>
2023-10-04 22:30:48 -0300

    index builder

---
+++ src/creational/builder/index.ts
@@ -0,0 +1,6 @@
+import { MainDishBuilder } from './classes/main-dish-builder';
+import { VeganDishBuilder } from './classes/vegan-dish-builder';
+
+const mainDishBuilder = new MainDishBuilder();
+mainDishBuilder.makeMeal();
+console.log(mainDishBuilder.getMeal());
