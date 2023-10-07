4be627fddb12b80cbac51049ba2a4148b70aed83
caian <caianpedroso@gmail.com>
2023-10-07 09:06:07 -0300

    Create prototype-deepcopy

---
+++ src/creational/prototype/prototype-deepcopy/intex.ts
@@ -0,0 +1,39 @@
+import { Prototype } from './interface.ts';
+export class Person implements Prototype {
+    public addresses: Address[] = [];
+
+    constructor(public name: string, public age: number) {}
+
+    clone(): Person {
+        const newObj = new Person(this.name, this.age);
+        newObj.addresses = this.addresses.map((item) => item.clone());
+        return newObj;
+    }
+
+    addAddress(address: Address): void {
+        this.addresses.push(address);
+    }
+}
+
+export class Address implements Prototype {
+    constructor(public street: string, public number: number) {}
+
+    clone(): Address {
+        return new Address(this.street, this.number);
+    }
+}
+
+const address1 = new Address('Av Paulista', 150);
+const person1 = new Person('Caian', 28);
+person1.addAddress(address1);
+const person2 = person1.clone();
+
+person1.addresses[0].street = 'Teste Teste';
+
+person2.name = 'Person2';
+console.log(person2);
+console.log(person2.addresses);
+
+console.log();
+console.log(person1);
+console.log(person1.addresses);
