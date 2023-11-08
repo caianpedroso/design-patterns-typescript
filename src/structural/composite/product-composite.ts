6bfd04bc4ff4a5f967d3e49af834bfa5afa5777e
Caian Egidio <caianpedroso@gmail.com>
2023-11-08 20:46:26 -0300

    test commit mac

--- src/structural/composite/product-composite.ts
+++ src/structural/composite/product-composite.ts
@@ -1,7 +1,7 @@
 export abstract class ProductComponent {
-    abstract getPrice(): number;
+  abstract getPrice(): number;
 
-    add(product: ProductComponent): void {}
+  add(product: ProductComponent): void {}
 
-    remove(product: ProductComponent): void {}
-}
+  remove(product: ProductComponent): void {}
+};
