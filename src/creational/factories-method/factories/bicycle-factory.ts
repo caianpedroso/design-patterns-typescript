efdd83293d5acc9c50a89b8a01f3e0eb3616a6c3
Caian Egidio <caianpedroso@gmail.com>
2023-12-31 22:28:06 -0300

    Atualizar o bicycle-factory.ts

--- src/creational/factories-method/factories/bicycle-factory.ts
+++ src/creational/factories-method/factories/bicycle-factory.ts
@@ -5,5 +5,5 @@ import { Bicycle } from "../vehicle/bicycle";
 export class BicycleFactory extends VehicleFactory {
     getVehicle(vehicleName: string): Vehicle {
         return new Bicycle(vehicleName);
-    }
-}
+    };
+};
